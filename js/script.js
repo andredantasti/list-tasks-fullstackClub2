7b5adb892d3238291f0d3c1e4692a044da23d077
Andre Dantas <andremoreirati@gmail.com>
2026-02-21 10:38:15 -0300

    Refazendo projeto Lista de Tarefas do curso Full Stack

---
+++ js/script.js
@@ -0,0 +1,57 @@
+
+const tarefaInput = document.querySelector("#tarefa");
+console.log(tarefaInput);
+
+const btnEnviar = document.querySelector("#botaoEnviar");
+
+const listTasks = document.querySelector(".list-tasks");
+
+btnEnviar.addEventListener("click", (e)=>{
+    e.preventDefault();
+
+    const valorTarefa = tarefaInput.value.trim();
+    if(valorTarefa === ""){
+        alert("Preencha o nome da Tarefa!");
+        return;
+    }
+
+    inserirTarefaExcluir(valorTarefa);
+    
+    tarefaInput.value = "";
+})
+
+const inserirTarefaExcluir = (valor) =>{
+    console.log(valor);
+    
+    const linha = document.createElement("li"); 
+    const btnExcluir = document.createElement("button");
+    const imgLixeira = document.createElement("img");
+    imgLixeira.src = "images/delete-icon.svg";
+    imgLixeira.alt = "Excluir";
+    imgLixeira.classList.add("icon-lixeira");
+
+    btnExcluir.classList.add("botaoExcluir");
+
+    listTasks.appendChild(linha);
+    linha.textContent = valor;
+    linha.appendChild(btnExcluir);
+    btnExcluir.appendChild(imgLixeira);
+
+    //EXCLUIR TAREFA
+    btnExcluir.addEventListener("click", () => {
+      listTasks.removeChild(linha);
+    });
+
+    linha.addEventListener("click", ()=>{
+        linha.classList.toggle("concluida");
+        linha.style.backgroundColor = "green";
+    })
+
+}
+
+
+
+
+
+
+
